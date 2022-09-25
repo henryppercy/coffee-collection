e5d114cae69a3b06da56abe9523281d77d20d641
Henry <henryppercy@gmail.com>
2022-09-25 13:39:16 +0100

    Add index.php and style sheets

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//require
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- font awesome -->
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- title -->
+    <title>The Coffee Catalogue</title>
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <!-- favicon -->
+    <!-- google fonts -->
+</head>
+<body>
+
+</body>
+</html>
