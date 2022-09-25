d95fccdfd6efbf47581c8df54abc080a860c7a42
Henry <henryppercy@gmail.com>
2022-09-25 22:51:27 +0100

    Created basic HTML and CSS

--- index.php
+++ index.php
@@ -12,11 +12,61 @@
     <title>The Coffee Catalogue</title>
     <!-- stylesheets -->
     <link rel="stylesheet" href="styles/normalize.css">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png">
     <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
+<main>
+    <section id="title">
+        <h1>THE <span class="brown-highlight">COFFEE</span> CATALOGUE</h1>
+        <h2>A Collection of Coffee from <span class="green-highlight">El Salvador</span> to  <span class="red-highlight">Ethiopia</span></h2>
+    </section>
+
+    <section class="add">
+        <h3 class="brown-highlight ">Explore the Coffee Below:</h3>
+        <h3 class="brown-highlight ">Add more </h3>
+    </section>
+
+    <section class="collection">
+        <div class="card">
+            <div class="card-image">
+                <h2>Ethiopia</h2>
+                <hr>
+                <h3>Natural</h3>
+            </div>
+            <p>Mead / Fig / White Grape</p>
+        </div>
+        <div class="card">
+            <div class="card-image">
+                <h2>Colombia</h2>
+                <hr>
+                <h3>Anaerobic</h3>
+            </div>
+            <p>White Grape / Honeysuckle / Cocoa</p>
+        </div>
+        <div class="card">
+            <div class="card-image">
+                <h2>EAST TIMOR </h2>
+                <hr>
+                <h3>Washed</h3>
+            </div>
+            <p>Red Grapefruit / White Grape / Biscoff Biscuit</p>
+        </div>
+    </section>
+
+    <section class="button">
+            <h3 class="brown-highlight">Back to top</h3>
+    </section>
+</main>
+
+<footer>
+    <p>© 2022 Henry Percy</p>
+</footer>
 
 </body>
 </html>
