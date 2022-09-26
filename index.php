7245d475d26cfead1e47fc253615a351c27ad01b
Henry <henryppercy@gmail.com>
2022-09-26 22:44:13 +0100

    Change to h1 element and navbar

--- index.php
+++ index.php
@@ -8,26 +8,28 @@ require_once('functions.php')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>The Coffee Catalogue</title>
     <link rel="stylesheet" href="styles/normalize.css">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/style.css">
     <link rel="shortcut icon" type="image/png" href="images/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Vidaloka&display=swap" rel="stylesheet">
 </head>
 <body>
-<header>
+<header id="top">
 <nav>
-    <a class="underline" href="#">Home</a>
-    <a class="underline" href="#">Add more coffee</a>
+    <a href="#top">Home</a>
+    <a href="#">Add more coffee</a>
 </nav>
 </header>
 <main>
-    <section id="title" class="title">
-        <h1>THE <span class="brown-highlight">COFFEE</span> CATALOGUE</h1>
+    <section class="title">
+        <h1 class="green-bg-highlight">The</h1>
+        <h1 class="brown-bg-highlight">Coffee</h1>
+        <h1 class="red-bg-highlight">Catalogue</h1>
     </section>
     <h2>A Collection of Coffee from <span class="green-highlight">El Salvador</span> to  <span class="red-highlight">Ethiopia</span></h2>
     <section>
-        <h3>Explore the Coffee Below</h3>
+        <h3>Please Explore the Coffee Below</h3>
     </section>
     <section class="collection">
         <?php
@@ -37,11 +39,11 @@ require_once('functions.php')
         ?>
     </section>
     <section class="button">
-            <a href="#title" class="underline">Back to top</a>
+        <a href="#top">Back to top</a>
     </section>
 </main>
 <footer>
-    <p>© 2022 Henry Percy</p>
+    <p>© 2022 | Henry Percy</p>
 </footer>
 </body>
 </html>
